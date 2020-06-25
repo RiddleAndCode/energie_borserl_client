a68253feade074733cb19696cb7933657cf5711e
Dogus Ural <dogus@riddleandcode.com>
2020-06-26 00:00:43 +0200

    Updated CLI with fixed IP and port value and automatic epoch time extraction from system time.

--- cli/energieborserlcli.go
+++ cli/energieborserlcli.go
@@ -4,24 +4,29 @@ import (
    "fmt"
    ebc "github.com/RiddleAndCode/energieclient"
    "os"
+   "strconv"
+	"time"
 )
+var ipAddress string = "192.168.20.77"
+var port string = "5555"
+
 func main() {
-   fmt.Print("Enter IP Address: ") //Print function is used to display output in same line
-   scanner := bufio.NewScanner(os.Stdin)
-   scanner.Scan() // use `for scanner.Scan()` to keep reading
-   ipAddress := scanner.Text()
-   fmt.Print("Enter Port: ")
-   scanner.Scan() // use `for scanner.Scan()` to keep reading
-   port := scanner.Text()
-   url := "http://" + ipAddress + ":" + port + "/transaction"
-   fmt.Println("URL:>", url)
-   for true {
-      fmt.Print("\nEnter Consumption: ") //Print function is used to display output in same line
-      var consumption string
-      scanner.Scan() // use `for scanner.Scan()` to keep reading
-      consumption = scanner.Text()
-      ebc.SendConsumptionandGetIPDBTX(consumption,ipAddress,port)
-   }
+	fmt.Print("Enter IP Address: ") //Print function is used to display output in same line
+	scanner := bufio.NewScanner(os.Stdin)
+	url := "http://" + ipAddress + ":" + port + "/transaction"
+	fmt.Println("URL:>", url)
+	for true {
+		fmt.Print("\nEnter Consumption: ") //Print function is used to display output in same line
+		var consumption string
+		scanner.Scan() // use `for scanner.Scan()` to keep reading
+		consumption = scanner.Text()
+		ebc.SendConsumptionandGetIPDBTX(consumption, getEpochTime(), ipAddress, port)
+	}
 }
+
+func getEpochTime() string {
+	return strconv.FormatInt((time.Now().Unix()), 10)
+}
+
 
 
